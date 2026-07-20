4eabed411ba3d0ae72233216e169423d886f9433
agnus2agy-cyber <agnus2agy@gmail.com>
2026-07-20 18:18:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from retail-banking-wealth-advisor!")
 
 
-if __name__ == "__main__":
-    main()
+from fastapi import FastAPI
+
+from routes.query_routes import router as query_router
+
+
+app = FastAPI(
+	title="Retail Banking & Wealth Advisory System",
+	version="1.0"
+)
+
+
+@app.get("/")
+def root():
+	return {
+    	"message": "Retail Banking & Wealth Advisory System"
+	}
+
+
+@app.get("/health")
+def health():
+	return {
+    	"status": "UP"
+	}
+
+
+app.include_router(query_router)
+
+
